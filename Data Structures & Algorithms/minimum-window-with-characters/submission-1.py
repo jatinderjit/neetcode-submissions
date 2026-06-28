559408d572c8fe35b420abcf5dd29345fb74aa33
Jatinderjit Singh <jatinderjit89@gmail.com>
2026-06-28 16:10:49 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        t_count = Counter(t)
+        s_count = Counter()
+        i = 0
+        min_len = len(s) + 1
+        window = (0, 0)
+        found = 0
+        for j, ch in enumerate(s):
+            s_count[ch] += 1
+            if s_count[ch] == t_count[ch]:
+                found += 1
+            while i <= j and (s[i] not in t_count or s_count[s[i]] > t_count[s[i]]):
+                s_count[s[i]] -= 1
+                i += 1
+            if found == len(t_count) and j - i + 1 < min_len:
+                window = (i, j + 1)
+                min_len = j - i + 1
+        return s[window[0]:window[1]]
